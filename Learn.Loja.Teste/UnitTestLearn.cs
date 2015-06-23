07b59bba0ac3d8bde63e31b2a94288b36f189c41
diogocpcunha <diogocpcunha@live.com>
2015-06-22 22:05:29 -0300

    video_git

---
+++ Learn.Loja.Teste/UnitTestLearn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learn.Loja.Teste
+{
+    [TestClass]
+    public class UnitTestLearn
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Learn.Loja.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Learn.Loja.Web.MvcApplication" Language="C#" %>
