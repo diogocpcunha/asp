f4014f6ac7797b224406ac34696e932e3c5a612e
diogocpcunha <diogocpcunha@live.com>
2015-06-22 22:48:35 -0300

    video_git

---
+++ Learn.Loja.Dominio/Entidades/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.Loja.Dominio.Entidades
+{
+    class Produto
+    {
+        private int id { get; set; }
+
+        private string nome { get; set; }
+
+        private string descricao { get; set; }
+
+        private decimal preco { get; set; }
+
+        public string categoria { get; set; }
+    }
+}
